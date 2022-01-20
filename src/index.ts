960758390b8189e7918412c50ae4d52c578fa691
Mavrick Laakso <laakso.mavrick@gmail.com>
2022-01-19 22:04:48 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello, world!")
+import fs from 'fs/promises';
+import path from 'path';
+
+try {
+    const filepath = path.join(__dirname, 'sample', '123456789');
+    const file = await fs.readFile(filepath);
+} catch (e)
+{
+    console.error(e);
+}
+
+// read file
+// map file to integers
